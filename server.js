d46923411f38f02f1a29123abeb9abe217ec51f0
seanarch <zhuqingyun@outlook.com>
2023-05-03 11:19:53 -0600

    small changes

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const cluster = require("cluster");
-const os = require("os");
 
 const app = express();
 
@@ -21,18 +19,10 @@ app.get("/", (req, res) => {
 
 app.get("/timer", (req, res) => {
   delay(9000);
-  res.send(`Ding ding ding! ${process.pid}`);
+  res.send(`Beep beep beep! ${process.pid}`);
 });
 
 console.log("Running server.js ...");
-if (cluster.isMaster) {
-  console.log("Master has been started...");
-  const NUM_WORKERS = os.cpus().length;
 
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log("Worker process started.");
-  app.listen(3000);
-}
+console.log("Worker process started.");
+app.listen(3000);
